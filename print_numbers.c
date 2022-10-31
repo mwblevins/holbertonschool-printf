bfc7f9796f75427f75e7cfae8bcc1719adc8541e
mwblevins <michaelwblevins@gmail.com>
2022-10-31 09:23:52 -0700

    betty compliant

--- print_numbers.c
+++ print_numbers.c
@@ -7,33 +7,29 @@
  */
 int print_number(va_list args)
 {
-        int n;
-        int di;
-        int len;
-        unsigned int num;
+	int n;
+	int di;
+	int len;
+	unsigned int num;
 
-        n  = va_arg(args, int);
-        di = 1;
-        len = 0;
+	n  = va_arg(args, int);
+	di = 1;
+	len = 0;
+	if (n < 0)
+	{
+		len += charput('-');
+		num = n * -1;
+	}
+	else
+		num = n;
 
-        if (n < 0)
-        {
-                len += charput('-');
-                num = n * -1;
-        }
-        else
-                num = n;
-
-        for (; num / di > 9; )
-                di *= 10;
-
-        for (; di != 0; )
-        {
-                len += charput('0' + num / di);
-                num %= di;
-                di /= 10;
-        }
-
-        return (len);
+	for (; num / di > 9; )
+		di *= 10;
+	for (; di != 0; )
+	{
+		len += charput('0' + num / di);
+		num %= di;
+		di /= 10;
+	}
+	return (len);
 }
-
